7d888b8a933ca408cccebd3291d32b4a5463fa99
Peleg Br <pelegbr@gmail.com>
2021-06-18 18:58:02 +0300

    Basic image output checks in resize_test.go

--- resize_test.go
+++ resize_test.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"testing"
 
-	"gocv.io/x/gocv"
+	"github.com/disintegration/imaging"
 )
 
-//https://images.pexels.com/photos/658687/pexels-photo-658687.jpeg?cs=srgb&dl=pexels-cindy-gustafson-658687.jpg&fm=jpg
 type testData struct {
 	url               string
 	verticalPadding   float32
@@ -23,7 +21,35 @@ type testData struct {
 }
 
 var baseUrl string
+
 var testCases = []testData{
+	{
+		url:               "file:///OgSquare.png",
+		verticalPadding:   0,
+		horizontalPadding: 0.025,
+		width:             200,
+		height:            200,
+		targetWidthRatio:  .65,
+		targetHeightRatio: .75,
+	},
+	{
+		url:               "file:///OgSquare.png",
+		verticalPadding:   0.025,
+		horizontalPadding: 0.05,
+		width:             200,
+		height:            200,
+		targetWidthRatio:  1.2,
+		targetHeightRatio: 1.1,
+	},
+	{
+		url:               "https://images.pexels.com/photos/658687/pexels-photo-658687.jpeg?cs=srgb&dl=pexels-cindy-gustafson-658687.jpg&fm=jpg",
+		verticalPadding:   0,
+		horizontalPadding: 0,
+		width:             2812,
+		height:            2250,
+		targetWidthRatio:  .8,
+		targetHeightRatio: .8,
+	},
 	{
 		url:               "https://images.pexels.com/photos/658687/pexels-photo-658687.jpeg?cs=srgb&dl=pexels-cindy-gustafson-658687.jpg&fm=jpg",
 		verticalPadding:   0,
@@ -76,13 +102,14 @@ func init() {
 	if port == "" {
 		port = "8080"
 	}
-	baseUrl = fmt.Sprintf("http://localhost:%s/", port)
+	baseUrl = fmt.Sprintf("http://localhost:%s/thumbnail", port)
 }
 
 // Test the service with a valid set of data for the correct cropped image behavior
 // Expect not to get an error
 func TestResize(t *testing.T) {
 	for i, testCase := range testCases {
+		t.Logf("Iter#%d", i)
 		var newWidth = int(float32(testCase.width) * testCase.targetWidthRatio)
 		var newHeight = int(float32(testCase.height) * testCase.targetHeightRatio)
 		var url = fmt.Sprintf("%s?url=%s&width=%d&height=%d", baseUrl, testCase.url, newWidth, newHeight)
@@ -92,72 +119,20 @@ func TestResize(t *testing.T) {
 			continue
 		}
 		if response.StatusCode != http.StatusOK {
-			t.Errorf("Unexpected error for URL: %s, errorStatus: %s", url, response.Status)
+			var res, _ = ioutil.ReadAll(response.Body)
+			t.Errorf("Unexpected error for URL: %s, errorStatus: %s, error: %s", url, response.Status, res)
 			continue
 		}
-		bytes, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			t.Errorf("Error reading response from service error: %s", err.Error())
-			continue
-		}
-		img, err := gocv.IMDecode(bytes, gocv.IMReadAnyColor)
+		img, err := imaging.Decode(response.Body)
 		if err != nil {
 			t.Errorf("Error reading image output of the resizer service, error: %s", err.Error())
 			continue
 		}
-		if img.Cols() != newWidth {
-			t.Errorf("Wrong width returned from service, expected: %d, actual: %d", newWidth, img.Cols())
+		if img.Bounds().Dx() != newWidth {
+			t.Errorf("Wrong width returned from service, expected: %d, actual: %d", newWidth, img.Bounds().Dx())
 		}
-		if img.Rows() != newHeight {
-			t.Errorf("Wrong height returned from service, expected: %d, actual: %d", newHeight, img.Rows())
-		}
-		if img.Channels() != 3 {
-			t.Errorf("The service responded with the wrong number of channels: %d instead of 3", img.Channels())
-		}
-		if img.Type() != gocv.MatTypeCV8SC3 {
-			img.ConvertTo(&img, gocv.MatTypeCV8SC3)
-		}
-
-		if byteScalarToInt(img.Sum()) < newWidth*newHeight*100 {
-			t.Errorf("Expected a non-blank image")
-		}
-		gocv.IMWrite(fmt.Sprintf("Test#%d.out.jpg", i), img)
-
-		// Check what area is supposed to be cropped
-		var topCroppedPixels = int(float32(testCase.height) * testCase.horizontalPadding)
-		var leftCroppedPixels = int(float32(testCase.width) * testCase.verticalPadding)
-		var bottomCropOffset = newHeight - int(float32(testCase.height)*testCase.horizontalPadding)
-		var rightCropOffset = newWidth - int(float32(testCase.width)*testCase.verticalPadding)
-		// Calculate the regions
-		var topCroppedRegion = img.Region(image.Rect(0, 0, newWidth, topCroppedPixels))
-		var leftCroppedRegion = img.Region(image.Rect(0, topCroppedPixels, leftCroppedPixels, bottomCropOffset))
-		var rightCroppedRegion = img.Region(image.Rect(rightCropOffset, topCroppedPixels, newWidth, bottomCropOffset))
-		var bottomCroppedRegion = img.Region(image.Rect(0, bottomCropOffset, newWidth, newHeight))
-		// Check the cropped area is black accommodating the JPG lose of color accuracy
-		if checkScalarAvg(topCroppedRegion) {
-			t.Errorf("Bad cropping, the top crop area has color")
-		}
-		if checkScalarAvg(leftCroppedRegion) {
-			t.Errorf("Bad cropping, the left crop area has color")
-		}
-		if checkScalarAvg(rightCroppedRegion) {
-			t.Errorf("Bad cropping, the right crop area has color")
-		}
-		if checkScalarAvg(bottomCroppedRegion) {
-			t.Errorf("Bad cropping, the bottom crop area has color")
+		if img.Bounds().Dy() != newHeight {
+			t.Errorf("Wrong height returned from service, expected: %d, actual: %d", newHeight, img.Bounds().Dy())
 		}
 	}
-}
-
-// Check the scalar sum of all pixel data doesn't pass more than 1 per pixel in average
-func checkScalarAvg(region gocv.Mat) bool {
-	var max = float64(region.Total())
-	var scalar = region.Sum()
-	fmt.Println(scalar)
-	return scalar.Val1 > max || scalar.Val2 > max || scalar.Val3 > max || scalar.Val4 > max
-}
-
-// Converts a scalar(B4) to an int
-func byteScalarToInt(scalar gocv.Scalar) int {
-	return int(scalar.Val1)<<24 | int(scalar.Val2)<<16 | int(scalar.Val3)<<8 | int(scalar.Val4)
 }
