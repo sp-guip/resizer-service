9e19c01713ecb25d9b567174749f9c8f3ac6767f
Peleg Br <pelegbr@gmail.com>
2021-06-18 22:54:27 +0300

    Refactored handling to separate logic and transport internal errors are now not exposed to the client

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"image"
@@ -21,8 +22,11 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", handleResizeImage)
-	http.ListenAndServe(":"+port, nil)
+	log.Println("Server listening on port: " + port)
+	http.HandleFunc("/thumbnail", handleResizeImage)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Error initializing server, err: %s", err.Error())
+	}
 }
 
 type jsonError struct {
@@ -36,9 +40,16 @@ func httpErrorF(res http.ResponseWriter, format string, args ...interface{}) {
 	}
 	msg, err := json.Marshal(jsonErr)
 	if err != nil {
-		log.Fatalf("Error formating a jsonError, error: %s", err.Error())
+		logErrorf("Error formating a jsonError, error: %s", err.Error())
 	}
+	logErrorf(jsonErr.Error)
 	res.Write(msg)
+}
+
+func logErrorf(err string, args ...interface{}) {
+	log.SetOutput(os.Stderr)
+	log.Printf(err, args...)
+	log.SetOutput(os.Stdout)
 }
 
 //Handle the images resize operations from
@@ -69,55 +80,62 @@ func handleResizeImage(res http.ResponseWriter, req *http.Request) {
 		httpErrorF(res, "malformed request URL, the height Parameter specified is illegal, URL: %s, Height: %s", req.URL.String(), heightParam)
 
 	} else {
-		imgData, err := getImage(urlParam)
+		img, err := fetchImage(urlParam)
 		if err != nil {
 			httpErrorF(res, err.Error())
 			return
 		}
-		var dstMat = gocv.NewMatWithSize(height, width, gocv.MatTypeCV8SC3)
-		var widthResizeRatio = float64(width) / float64(imgData.Cols())
-		var heightResizeRatio = float64(height) / float64(imgData.Rows())
-		var minResizeRatio = widthResizeRatio
-		if widthResizeRatio > heightResizeRatio {
-			minResizeRatio = heightResizeRatio
-		}
-		if minResizeRatio > 1 {
-			minResizeRatio = 1
-		}
-		var size = image.Point{X: int(minResizeRatio * float64(imgData.Cols())), Y: int(minResizeRatio * float64(imgData.Rows()))}
-		var xOffset = (dstMat.Cols() - size.X) / 2
-		var yOffset = (dstMat.Rows() - size.Y) / 2
-		var cropOffset = image.Rect(xOffset, yOffset, dstMat.Cols()-xOffset, dstMat.Rows()-yOffset)
-		gocv.Resize(*imgData, imgData, size, 0, 0, gocv.InterpolationNearestNeighbor)
-		// replacing, due to gocv bug
-		// https://www.gitmemory.com/issue/hybridgroup/gocv/387/456821537
-		// var dstROI = dstMat.Region(cropOffset)
-		// imgData.CopyTo(&dstROI)
-		if imgData.Type() != gocv.MatTypeCV8SC3 {
-			imgData.ConvertTo(imgData, gocv.MatTypeCV8SC3)
-		}
-		imgPixels, _ := imgData.DataPtrInt8()
-		dstPixels, _ := dstMat.DataPtrInt8()
-		for i := 0; i < size.Y; i++ {
-			copy(dstPixels[((i+cropOffset.Min.Y)*width+cropOffset.Min.X)*3:((i+cropOffset.Min.Y)*width+cropOffset.Max.X)*3], imgPixels[i*size.X*3:(i+1)*size.X*3])
-		}
-
-		outputBytes, err := gocv.IMEncode(gocv.JPEGFileExt, dstMat)
+		output, err := resizeAndCrop(*img, width, height)
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
 			res.Write([]byte(fmt.Sprintf("error trying to convert the result image to bytes, err: %s", err.Error())))
 		} else {
 			res.WriteHeader(http.StatusOK)
-			if _, err := res.Write(outputBytes); err != nil {
+			if _, err := res.Write(output.Bytes()); err != nil {
 				log.Fatalf("error writing image response, err: %s", err.Error())
 			}
 		}
 	}
 }
 
+func resizeAndCrop(img gocv.Mat, width, height int) (bytes.Buffer, error) {
+	var dstMat = gocv.NewMatWithSize(height, width, gocv.MatTypeCV8SC3)
+	var widthResizeRatio = float64(width) / float64(img.Cols())
+	var heightResizeRatio = float64(height) / float64(img.Rows())
+	var minResizeRatio = widthResizeRatio
+
+	if widthResizeRatio > heightResizeRatio {
+		minResizeRatio = heightResizeRatio
+	}
+	if minResizeRatio > 1 {
+		minResizeRatio = 1
+	}
+
+	var size = image.Point{X: int(minResizeRatio * float64(img.Cols())), Y: int(minResizeRatio * float64(img.Rows()))}
+	var xOffset = (dstMat.Cols() - size.X) / 2
+	var yOffset = (dstMat.Rows() - size.Y) / 2
+	var cropOffset = image.Rect(xOffset, yOffset, dstMat.Cols()-xOffset, dstMat.Rows()-yOffset)
+	gocv.Resize(img, &img, size, 0, 0, gocv.InterpolationNearestNeighbor)
+	// replacing, due to gocv bug
+	// https://www.gitmemory.com/issue/hybridgroup/gocv/387/456821537
+	// var dstROI = dstMat.Region(cropOffset)
+	// imgData.CopyTo(&dstROI)
+	if img.Type() != gocv.MatTypeCV8SC3 {
+		img.ConvertTo(&img, gocv.MatTypeCV8SC3)
+	}
+	imgPixels, _ := img.DataPtrInt8()
+	dstPixels, _ := dstMat.DataPtrInt8()
+	for i := 0; i < size.Y; i++ {
+		copy(dstPixels[((i+cropOffset.Min.Y)*width+cropOffset.Min.X)*3:((i+cropOffset.Min.Y)*width+cropOffset.Max.X)*3], imgPixels[i*size.X*3:(i+1)*size.X*3])
+	}
+
+	outputBytes, err := gocv.IMEncode(gocv.JPEGFileExt, dstMat)
+	return *bytes.NewBuffer(outputBytes), err
+}
+
 // Fetch and parse the image from the url
 // Varifying it's validity in the end
-func getImage(url string) (*gocv.Mat, error) {
+func fetchImage(url string) (*gocv.Mat, error) {
 	var err error
 	var input io.Reader
 	if strings.Index(url, "file:///") == 0 {
@@ -129,11 +147,13 @@ func getImage(url string) (*gocv.Mat, error) {
 	}
 	encodedBytes, err := ioutil.ReadAll(input)
 	if err != nil {
-		return nil, fmt.Errorf("error reading image data from url: %s, err: %w", url, err)
+		logErrorf("unable to get image from url: %s, err: %s", url, err.Error())
+		return nil, fmt.Errorf("unable to get image from url: %s", url)
 	}
 	img, err := gocv.IMDecode(encodedBytes, gocv.IMReadAnyColor)
 	if err != nil || img.Empty() {
-		return nil, fmt.Errorf("error decoding image data from url: %s, err: %w", url, err)
+		logErrorf("error decoding image data from url: %s, err: %s", url, err.Error())
+		return nil, fmt.Errorf("error decoding image data from url: %s", url)
 	}
 	return &img, nil
 }
