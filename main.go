6a25db9f844518a5c1af8bc758d3e1c294743e99
Peleg Br <pelegbr@gmail.com>
2021-06-18 22:36:53 +0300

    Refactored handling to separate logic and transport internal errors are now not exposed to the client

--- main.go
+++ main.go
@@ -23,8 +23,11 @@ func main() {
 		port = "8080"
 	}
 
+	log.Println("Server listening on port: " + port)
 	http.HandleFunc("/thumbnail", handleResizeImage)
-	http.ListenAndServe(":"+port, nil)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Error initializing server, err: %s", err.Error())
+	}
 }
 
 type jsonError struct {
@@ -38,9 +41,16 @@ func httpErrorF(res http.ResponseWriter, format string, args ...interface{}) {
 	}
 	msg, err := json.Marshal(jsonErr)
 	if err != nil {
-		log.Fatalf("Error formating a jsonError, error: %s", err.Error())
+		logErrorf("Error formating a jsonError, error: %s", err.Error())
 	}
+	logErrorf(jsonErr.Error)
 	res.Write(msg)
+}
+
+func logErrorf(err string, args ...interface{}) {
+	log.SetOutput(os.Stderr)
+	log.Printf(err, args...)
+	log.SetOutput(os.Stdout)
 }
 
 //Handle the images resize operations from
@@ -71,43 +81,47 @@ func handleResizeImage(res http.ResponseWriter, req *http.Request) {
 		httpErrorF(res, "malformed request URL, the height Parameter specified is illegal, URL: %s, Height: %s", req.URL.String(), heightParam)
 
 	} else {
-		imgData, err := getImage(urlParam)
+		img, err := fetchImage(urlParam, res)
 		if err != nil {
 			httpErrorF(res, err.Error())
-			return
 		}
-		var originalWidth = imgData.Bounds().Dx()
-		var originalHeight = imgData.Bounds().Dy()
-
-		var widthResizeRatio = float64(width) / float64(originalWidth)
-		var heightResizeRatio = float64(height) / float64(originalHeight)
-		var minResizeRatio = math.Min(math.Min(widthResizeRatio, heightResizeRatio), 1)
-
-		var size = image.Point{X: int(minResizeRatio * float64(originalWidth)), Y: int(minResizeRatio * float64(originalHeight))}
-		var resizedImage = imaging.Resize(imgData, size.X, size.Y, imaging.BSpline)
-
-		var dstImage = imaging.New(width, height, color.Black)
-		var xOffset = (width - size.X) / 2
-		var yOffset = (height - size.Y) / 2
-		dstImage = imaging.Paste(dstImage, resizedImage, image.Point{X: xOffset, Y: yOffset})
-
-		var buf = bytes.Buffer{}
-		err = imaging.Encode(&buf, dstImage, imaging.JPEG, imaging.JPEGQuality(100))
+		buf, err := resizeAndCrop(img, width, height)
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
-			res.Write([]byte(fmt.Sprintf("error trying to convert the result image to bytes, err: %s", err.Error())))
+			logErrorf(fmt.Sprintf("error trying to encode the image, err: %s", err.Error()))
+			res.Write([]byte("error trying to encode the image"))
 		} else {
 			res.WriteHeader(http.StatusOK)
 			if _, err := res.Write(buf.Bytes()); err != nil {
-				log.Fatalf("error writing image response, err: %s", err.Error())
+				logErrorf("error writing image response, err: %s", err.Error())
 			}
 		}
 	}
 }
 
+func resizeAndCrop(img image.Image, width, height int) (bytes.Buffer, error) {
+	var originalWidth = img.Bounds().Dx()
+	var originalHeight = img.Bounds().Dy()
+
+	var widthResizeRatio = float64(width) / float64(originalWidth)
+	var heightResizeRatio = float64(height) / float64(originalHeight)
+	var minResizeRatio = math.Min(math.Min(widthResizeRatio, heightResizeRatio), 1)
+
+	var size = image.Point{X: int(minResizeRatio * float64(originalWidth)), Y: int(minResizeRatio * float64(originalHeight))}
+	var resizedImage = imaging.Resize(img, size.X, size.Y, imaging.BSpline)
+
+	var dstImage = imaging.New(width, height, color.Black)
+	var xOffset = (width - size.X) / 2
+	var yOffset = (height - size.Y) / 2
+	dstImage = imaging.Paste(dstImage, resizedImage, image.Point{X: xOffset, Y: yOffset})
+
+	var buf = bytes.Buffer{}
+	return buf, imaging.Encode(&buf, dstImage, imaging.JPEG, imaging.JPEGQuality(100))
+}
+
 // Fetch and parse the image from the url
 // Varifying it's validity in the end
-func getImage(url string) (image.Image, error) {
+func fetchImage(url string, res http.ResponseWriter) (image.Image, error) {
 	var err error
 	var input io.Reader
 	if strings.Index(url, "file:///") == 0 {
@@ -118,11 +132,13 @@ func getImage(url string) (image.Image, error) {
 		input = req.Body
 	}
 	if err != nil {
-		return nil, fmt.Errorf("unable to get image from url: %s, err: %w", url, err)
+		logErrorf("unable to get image from url: %s, err: %s", url, err.Error())
+		return nil, fmt.Errorf("unable to get image from url: %s", url)
 	}
 	img, err := imaging.Decode(input)
 	if err != nil {
-		return nil, fmt.Errorf("error decoding image data from url: %s, err: %w", url, err)
+		logErrorf("error decoding image data from url: %s, err: %s", url, err.Error())
+		return nil, fmt.Errorf("error decoding image data from url: %s", url)
 	}
 	return img, nil
 }
